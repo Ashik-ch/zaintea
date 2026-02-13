36e44f6ad3396060ecf0bba17be16112efbd7610
Ashik-ch <ashikch7117@gmail.com>
2026-02-13 12:29:28 +0400

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/zaintea/',
+  base: '/',
   plugins: [react(),
   tailwindcss(),
   ],
