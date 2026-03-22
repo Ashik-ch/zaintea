54dc89d93e6dc1781f65be818e828c2a2f1f27a3
Ashik-ch <ashikch7117@gmail.com>
2026-03-22 11:52:37 +0400

    site map

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(),
-  tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          motion: ['framer-motion'],
+        }
+      }
+    }
+  }
 })
-
