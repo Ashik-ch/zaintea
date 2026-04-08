21ad009f848e07e60a97b92e07759659699cdcd7
Ashik-ch <ashikch7117@gmail.com>
2026-04-08 12:13:35 +0400

    tv menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Landing from './components/Landing';
 import PrayerTimes from './components/PrayerTimes';
-import TvDisplay from './components/TvDisplay'; // New TvDisplay imported
-import ZainTeaMenu from './components/ZainTeaMenu';
+import TvDisplay from './components/TvDisplay';  
+import TvMenu from './components/tvMenu';
+import Testimonials from './components/Testimonials';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />} />
+      <Route path="/reviews" element={<Testimonials />} />
       <Route path="/landing" element={<Landing />} />
       <Route path="/prayer-times" element={<PrayerTimes />} />
-      <Route path="/sandwiches" element={<ZainTeaMenu />} />
+      <Route path="/sandwiches" element={<TvMenu />} />
+      <Route path="/tv-menu" element={<TvMenu />} />
       <Route path="/tv-display" element={<TvDisplay />} />
     </Routes>
   );
